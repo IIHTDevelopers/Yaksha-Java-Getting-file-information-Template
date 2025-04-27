b0babd1f001a29fd7bced7526638aadf0c921632
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 18:49:24 +0530

    Update FileInfo.java

--- src/main/java/com/yaksha/assignment/FileInfo.java
+++ src/main/java/com/yaksha/assignment/FileInfo.java
@@ -6,31 +6,19 @@ public class FileInfo {
 
 	public static void main(String[] args) {
 		// **Create a file instance**
-		File file = new File("fileInfo.txt");
 
 		// Check if the file exists
-		if (file.exists()) {
-			System.out.println("File Information:");
 
-			// **Get file name**
-			System.out.println("File Name: " + file.getName());
+		// **Get file name**
 
-			// **Get file path**
-			System.out.println("File Path: " + file.getAbsolutePath());
+		// **Get file path**
 
-			// **Get file size**
-			System.out.println("File Size: " + file.length() + " bytes");
+		// **Get file size**
 
-			// **Check if the file is readable**
-			System.out.println("Is Readable: " + file.canRead());
+		// **Check if the file is readable**
 
-			// **Check if the file is writable**
-			System.out.println("Is Writable: " + file.canWrite());
+		// **Check if the file is writable**
 
-			// **Check if the file is a directory**
-			System.out.println("Is Directory: " + file.isDirectory());
-		} else {
-			System.out.println("File does not exist.");
-		}
+		// **Check if the file is a directory**
 	}
 }
